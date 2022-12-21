f6350b0f688fc9f431d240217b86b62ca4378631
Srijan-1-3 <srijanmohan1@gmail.com>
2022-12-21 09:59:40 +0530

    Initial state

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+function Navbar(){
+return(
+    <nav className="navbar bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+    
+      Summarizer
+    </a>
+  </div>
+</nav>
+);
+}
+
+export default Navbar;
