4f8ecac70011819f13e9a9a3c4331415512195d7
siddhant-afk <siddhantdudhane21@gmail.com>
2022-12-22 10:22:14 +0530

    Add button to textbox

--- client/src/components/TextBox.jsx
+++ client/src/components/TextBox.jsx
@@ -8,8 +8,13 @@ function TextBox(){
 <form action="" className=" mx-auto">
 
 <textarea name="" id="" cols="133" rows="10" placeholder="Enter Text or Article.." className="mx-auto form px-4 py-3"></textarea>
+
+<button type="submit" className="button">Summarize</button>
 </form>
     </div>
+
+
+
 
    
  );
