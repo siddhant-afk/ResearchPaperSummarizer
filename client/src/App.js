28baa4731b1512871853948dc7afa2c2baa2a4be
siddhant-afk <siddhantdudhane21@gmail.com>
2022-12-22 10:11:10 +0530

    TextBox ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
-
+import TextBox from "./components/TextBox";
 
 function App() {
   return (
+   <div>
     <Navbar />
+    <TextBox />
+   </div>
+    
   );
 }
 
---
+++ client/src/components/TextBox.jsx
@@ -0,0 +1,19 @@
+
+
+function TextBox(){
+ 
+ return(
+    <div className="container">
+
+<form action="" className=" mx-auto">
+
+<textarea name="" id="" cols="133" rows="10" placeholder="Enter Text or Article.." className="mx-auto form px-4 py-3"></textarea>
+</form>
+    </div>
+
+   
+ );
+  
+}
+
+export default TextBox;
