ee63a0ebc25a70f2f13af9389ab154028b7896d1
siddhant-afk <siddhantdudhane21@gmail.com>
2022-12-23 09:35:43 +0530

    Add Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
 import TextBox from "./components/TextBox";
-
+import Footer from "./components/Footer";
 function App() {
   return (
    <div>
     <Navbar />
     <TextBox />
+    <Footer />
    </div>
     
   );
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+function Footer(){
+
+    return(
+         
+        <div className="container text-center footer">
+            <hr />
+            Copyrights ©️ 2023 : Siddhant Dudhane
+        </div>
+     
+    );
+}
+
+export default Footer;
