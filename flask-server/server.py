24216bd682e222f5a66d8943b19cbbed7ec172b2
siddhant-afk <siddhantdudhane21@gmail.com>
2022-12-30 09:55:26 +0530

    Create flask server

---
+++ flask-server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/summary")
+def  summarize():
+    return {"summary": " My name is Yoshikage Kira. I’m 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don’t smoke, but I occasionally drink. I’m in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning." }
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)    
